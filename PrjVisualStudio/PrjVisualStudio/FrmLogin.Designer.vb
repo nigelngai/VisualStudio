e351ae7f59d6361ff1ab35805c01349df3845d50
nigelngai <turbosolo@hotmail.com>
2016-09-12 13:21:06 +0800

    Create FrmLogin

--- PrjVisualStudio/PrjVisualStudio/FrmLogin.Designer.vb
+++ PrjVisualStudio/PrjVisualStudio/FrmLogin.Designer.vb
@@ -22,9 +22,17 @@ Partial Class FrmLogin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'FrmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Name = "FrmLogin"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
