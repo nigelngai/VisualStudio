500ccb554e6e2b3adea4f350656995707dae184f
nigelngai <turbosolo@hotmail.com>
2016-09-13 10:39:15 +0800

    Basic

---
+++ PrjVisualStudio/PrjVisualStudio/Date/ModDate.vb
@@ -0,0 +1,111 @@
+﻿Module ModDate
+
+
+    Public Function GetMyDateFormat(ByRef myPicker As DateTimePicker) As String
+
+        Dim strYear As String = ""
+        Dim strMonth As String = ""
+        Dim strDate As String = ""
+
+        strYear = Format(myPicker.Value, "yyyy")
+        strMonth = GetMonthNamebyNumber(Format(myPicker.Value, "MM"))
+        strDate = Format(myPicker.Value, "dd")
+
+        GetMyDate = strYear & " " & strMonth & " " & strDate
+
+    End Function
+
+    Public Function GetMonthNamebyNumber(ByRef strMonth As String) As String
+
+        GetMonthNamebyNumber = ""
+
+        If Strings.Len(strMonth) = 1 Then strMonth = "0" & strMonth
+
+        Select Case strMonth
+
+            Case "01"
+                GetMonthNamebyNumber = "Jan"
+            Case "02"
+                GetMonthNamebyNumber = "Feb"
+            Case "03"
+                GetMonthNamebyNumber = "Mar"
+            Case "04"
+                GetMonthNamebyNumber = "Apr"
+            Case "05"
+                GetMonthNamebyNumber = "May"
+            Case "06"
+                GetMonthNamebyNumber = "Jun"
+            Case "07"
+                GetMonthNamebyNumber = "Jul"
+            Case "08"
+                GetMonthNamebyNumber = "Aug"
+            Case "09"
+                GetMonthNamebyNumber = "Sep"
+            Case "10"
+                GetMonthNamebyNumber = "Oct"
+            Case "11"
+                GetMonthNamebyNumber = "Nov"
+            Case "12"
+                GetMonthNamebyNumber = "Dec"
+
+        End Select
+
+        Debug.Print(GetMonthNamebyNumber)
+
+    End Function
+
+
+    Public Function getThisMonthStart(ByVal pDate As Date) As Date
+
+        Dim rDate As Date
+        rDate = New Date(pDate.Year, pDate.Month, 1)
+        '--->
+        Return rDate
+
+    End Function
+
+    Public Function getThisMonthEnd(ByVal pDate As Date) As Date
+
+        Dim rDate As Date
+        rDate = New Date(pDate.Year, pDate.Month, 1)
+        rDate = rDate.AddMonths(1)
+        rDate = rDate.AddDays(-1)
+        '--->
+        Return rDate
+
+    End Function
+
+    Public Function getLastMonthStart(ByVal pDate As Date) As Date
+
+        Dim rDate As Date
+        rDate = New Date(pDate.Year, pDate.Month, 1)
+        rDate = rDate.AddMonths(-1)
+        '--->
+        Return rDate
+
+    End Function
+
+    Public Function getLastMonthEnd(ByVal pDate As Date) As Date
+
+        Dim rDate As Date
+        rDate = New Date(pDate.Year, pDate.Month, 1)
+        rDate = rDate.AddDays(-1)
+        '--->
+        Return rDate
+
+    End Function
+
+
+    'Private Sub ButtonGet_Click(sender As System.Object, e As System.EventArgs) Handles ButtonGet.Click
+
+    '    Dim SourceDate As Date = ("2016-05-15")
+
+    '    Dim TargetDate As Date
+    '    TargetDate = getLastMonthEnd(SourceDate)
+    '    Debug.Print(Format(TargetDate, "yyyy-MM-dd"))
+
+    'End Sub
+
+
+
+End Module
---
+++ PrjVisualStudio/PrjVisualStudio/GridView/ModGrid.vb
@@ -0,0 +1,107 @@
+﻿Module ModGrid
+
+
+
+    Public Function CalculateGridValueByColumnName(ByVal GridDetail As DataGridView, ByVal ColumnName As String) As Double
+
+        Dim dblQty As Double
+        dblQty = 0
+
+        Try
+
+            Dim i As Integer
+
+            For i = 0 To GridDetail.RowCount - 1
+
+                If GridDetail.Rows(i).Cells(ColumnName).ToString <> "" Then
+
+                    If Not IsNumeric(GridDetail.Rows(i).Cells(ColumnName).Value) Then GridDetail.Rows(i).Cells(ColumnName).Value = 0
+                    dblQty = dblQty + GridDetail.Rows(i).Cells(ColumnName).Value
+                End If
+
+            Next
+
+            Return dblQty
+
+        Catch ex As Exception
+
+            CalculateGridValueByColumnName = 0
+
+        End Try
+
+    End Function
+
+
+    Public Sub ModifyGridSettingAzure(ByVal Grid As DataGridView)
+
+        Try
+
+            Grid.RowHeadersWidth = 42
+            Grid.Columns(0).Frozen = True
+            Grid.AlternatingRowsDefaultCellStyle.BackColor = Color.LightYellow
+            Grid.AllowUserToAddRows = False
+            Grid.AllowUserToDeleteRows = False
+            Grid.EditMode = DataGridViewEditMode.EditProgrammatically
+
+            Grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            Grid.MultiSelect = False
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+
+    Public Sub ModifyGridSettingSelfDefine(ByVal Grid As DataGridView, ByVal MyColor As Color)
+
+        Try
+
+            Grid.RowHeadersWidth = 25
+            Grid.Columns(0).Frozen = True
+            Grid.AlternatingRowsDefaultCellStyle.BackColor = MyColor
+            Grid.AllowUserToAddRows = False
+            Grid.AllowUserToDeleteRows = False
+            Grid.EditMode = DataGridViewEditMode.EditProgrammatically
+
+            Grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            Grid.MultiSelect = False
+
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+
+    Public Sub ModifyGridDateColumn(ByVal Grid As DataGridView, ByVal ColumnName As String)
+        Try
+
+            Dim MyStyle As New DataGridViewCellStyle()
+            MyStyle.Format = "yyyy-MM-dd"
+            Grid.Columns(ColumnName).DefaultCellStyle = MyStyle
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+
+    Public Sub ModifyGridDateColumnInLong(ByVal Grid As DataGridView, ByVal ColumnName As String)
+        Try
+
+            Dim MyStyle As New DataGridViewCellStyle()
+            MyStyle.Format = "yyyy-MM-dd HH:mm:ss"
+            Grid.Columns(ColumnName).DefaultCellStyle = MyStyle
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+
+
+End Module
