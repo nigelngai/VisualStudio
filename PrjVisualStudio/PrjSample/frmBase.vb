9b8cbc658a7e02455c6f78f2c0b52e62f7f5ac82
nigelngai <turbosolo@hotmail.com>
2016-09-15 08:04:14 +0800

    Folder

---
+++ PrjVisualStudio/PrjSample/frmBase.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.SqlClient
+
+
+Public Class frmBase
+
+    Private Sub frmBase_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+
+
+
+
+
+
+
+
+End Class
